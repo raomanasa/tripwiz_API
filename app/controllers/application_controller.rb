a9f0587c1d86353337637de033aa7dd82ab13ff8
Manasa Rao <drmanasa18.rao@gmail.com>
2020-02-06 11:00:06 +0100

    adds device ,user model and gem omniauth-facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
-end
+        include DeviseTokenAuth::Concerns::SetUserByToken
+        #protect_from_forgery unless: -> { request.format.json? }
+end   
