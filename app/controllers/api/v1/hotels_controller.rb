865a2a9434938e721eb217d9fafdc1c03dd402fd
mantanquzzmo <quzzmo@hotmail.com>
2020-02-10 14:11:33 +0100

    adds index action for hotels controller

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class Api::V1::HotelsController < ApplicationController
-  
   def create
     activity_type = ActivityType.find_by(trip_id: params[:trip])
-    
+
     if activity_type.activities != []
       lats = []
       longs = []
@@ -15,8 +16,8 @@ class Api::V1::HotelsController < ApplicationController
       lat = avg_lat.round(5)
       lng = avg_lng.round(5)
     end
-    
-    getHotels = get_hotels(params, lat, lng) 
+
+    getHotels = get_hotels(params, lat, lng)
     hotels = []
 
     if getHotels != 'error'
@@ -27,7 +28,7 @@ class Api::V1::HotelsController < ApplicationController
         price = hotel['offers'][0]['price']['total']
         address = hotel['hotel']['address']['lines'][0]
         url = hotel['offers'][0]['room']['description']['text']
-        
+
         hotel = Hotel.create(
           name: name,
           lat: lat,
@@ -45,23 +46,27 @@ class Api::V1::HotelsController < ApplicationController
     if hotels != []
       render json: hotels
     else
-      render json: { error: "No hotels found, try a different hotel rating or location."}, status: 400
+      render json: { error: 'No hotels found, try a different hotel rating or location.' }, status: 400
     end
+  end
+
+  def index
+    hotels = Hotel.where(trip_id: params[:trip])
+    render json: hotels
   end
 
   private
 
   def get_hotels(params, lat, lng)
-    amadeus = Amadeus::Client.new({hostname: :production,
-      client_id: Rails.application.credentials.client_id,
-      client_secret: Rails.application.credentials.client_secret
-    })
+    amadeus = Amadeus::Client.new(hostname: :production,
+                                  client_id: Rails.application.credentials.client_id,
+                                  client_secret: Rails.application.credentials.client_secret)
     begin
       response = amadeus.shopping.hotel_offers.get(latitude: lat, longitude: lng, ratings: params[:budget], radius: 3, radiusUnit: 'KM', view: 'LIGHT')
-    rescue Amadeus::ResponseError => error
+    rescue Amadeus::ResponseError => e
       return 'error'
     end
     hotels = JSON.parse(response.body)
     hotelsFinal = hotels['data'].slice(0, 3)
-  end 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :trips, only: [:create]
-      resources :hotels, only: [:create]
+      resources :hotels, only: %i[create index]
       resources :activity_types, only: [:create]
     end
   end
