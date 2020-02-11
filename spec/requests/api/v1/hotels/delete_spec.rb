1a0cc9198d5863d07f8e72cc80a2eb22c008456b
mantanquzzmo <quzzmo@hotmail.com>
2020-02-11 10:44:22 +0100

    rewrites destroy function not to delete all hotels

--- spec/requests/api/v1/hotels/delete_spec.rb
+++ spec/requests/api/v1/hotels/delete_spec.rb
@@ -3,15 +3,26 @@
 RSpec.describe 'DELETE /api/v1/hotels', type: :request do
   describe 'Hotel suggestions' do
     let!(:trip) { create(:trip) }
+    let!(:other_trip) { create(:trip) }
     3.times do
-      let!(:hotel) { create_list(:hotel, 3, trip_id: trip.id) }
+      let!(:trip_hotels) { create_list(:hotel, 3, trip_id: trip.id) }
+      let!(:other_trip_hotels) { create_list(:hotel, 3, trip_id: other_trip.id) }
     end
 
     it 'that are not chosen are deleted' do
       delete '/api/v1/hotels',
-             params: { hotel_id: trip.hotels.first.id }
+             params: { hotel_id: trip.hotels.first.id,
+            trip: trip.id }
+            
+      expect(trip.hotels.length).to eq 1
+    end
 
-      expect(response.status).to eq 200
+    it 'that belong to other trips are not deleted' do
+      delete '/api/v1/hotels',
+             params: { hotel_id: trip.hotels.first.id,
+              trip: trip.id }
+
+      expect(other_trip.hotels.empty?).to eq false
     end
   end
 end
