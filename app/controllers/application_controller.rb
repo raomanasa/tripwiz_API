bd223e4914dad63838a4a08f4a580815c01783e4
Manasa Rao <drmanasa18.rao@gmail.com>
2020-02-11 11:00:18 +0100

    adds changes in application controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
-  rescue_from ActionController::ParameterMissing, with: :missing_params
+      include DeviseTokenAuth::Concerns::SetUserByToken
+      rescue_from ActionController::ParameterMissing, with: :missing_params
 
   def missing_params
     render json: { error: "Missing parameters" }, status: 422
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe User, type: :model do
   describe 'Database table' do
     it { is_expected.to have_db_column :encrypted_password }
