de143bb6a34024518dfd1aa3778e0fbbe720b1a5
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-02-11 11:07:02 +0100

    Fix indentation and adds validation of tokens column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::API
-      include DeviseTokenAuth::Concerns::SetUserByToken
-      rescue_from ActionController::ParameterMissing, with: :missing_params
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  rescue_from ActionController::ParameterMissing, with: :missing_params
 
   def missing_params
-    render json: { error: "Missing parameters" }, status: 422
+    render json: { error: 'Missing parameters' }, status: 422
   end
 end
